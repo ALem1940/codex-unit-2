dfc0baa7ea103000e06e8202013367604a41de1d
Ashanti <ashantilemonia@gmail.com>
2026-04-30 10:25:10 -0500

    completed L4 L7

--- lesson-04-js-objects/level-07/update.js
+++ lesson-04-js-objects/level-07/update.js
@@ -1,4 +1,12 @@
 // TODO: Update existing properties and export the updated object.
-const obj = { age: 0 };
+const student = { 
+    age: 0 
+    name: "Felicia",
+    lastname: "Bye"
+};
+console.log(student.age); 
+student.age = 25;
+console.log(student.age);
 // TODO: update obj.age
-export default obj;
+export default student;
+ 
