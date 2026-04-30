a22bea0b591eca640b76273fb515f41573563800
Ashanti <ashantilemonia@gmail.com>
2026-04-30 03:58:56 -0500

    completed L4-L1

--- lesson-04-js-objects/level-01/description.js
+++ lesson-04-js-objects/level-01/description.js
@@ -1,3 +1,4 @@
 // TODO: Default-export a short description string about objects.
 // Example: export default "Objects are key/value collections."
-export default "TODO: A short description of objects.";
+export default "JavaScript objects are key/value collections that contain different types of data like arrays. They access data by name.";
+
--- lesson-04-js-objects/level-01/examples.js
+++ lesson-04-js-objects/level-01/examples.js
@@ -1,3 +1,4 @@
 // TODO: Default-export an array of two short example strings.
 // Example: export default ["{ a: 1 }", "{ name: 'Ava' }"]
-export default ["TODO example 1", "TODO example 2"];
+export default ["JavaScript objects are key/value collections that contain different types of data like arrays.",
+ "They access data by name"];
