2c85fc36211f390f60a534b2bf0afa46e4a565a3
Ashanti <ashantilemonia@gmail.com>
2026-05-02 11:47:01 -0500

    answered L5 l2

--- lesson-05-terminal-commands/level-02/answer.js
+++ lesson-05-terminal-commands/level-02/answer.js
@@ -1,3 +1,3 @@
 // Level 02 — List files with ls
 // TODO: Run `ls` in the terminal and write a short note here about what you saw.
-export default "";
+export default "Running ls shows the folders in a blue color: file 1-10 shown.";
