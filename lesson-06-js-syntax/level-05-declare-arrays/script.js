3a7bffbf29b01766092a90cf0e4a812135c499f6
Ashanti <ashantilemonia@gmail.com>
2026-05-03 06:22:02 -0500

    finalized L6 L5

--- lesson-06-js-syntax/level-05-declare-arrays/script.js
+++ lesson-06-js-syntax/level-05-declare-arrays/script.js
@@ -4,4 +4,15 @@
 // Example:
 // let userNames = ["Ava", "Jon", "Maya"];
 
-let userNames = [];
+let userNames = ["Pearla", "Amber", "Don"];
+let allStarStudents = ["Mariah", "Kimberly", "Christopher"];
+let bonusPlayers = ["Joseph", "Yana","Rolazar",];
+let teamWildCards = ["Jennifer","Chris","Jose"];
+let extraCards = ["Tom","Alesandra", "Ashanti"];
+let userAges = [20,30,40];
+let needPoints = [5,10,15];
+let coinsAccessed = [7, 15,20];
+let goldCards = [70,80,90];
+let silverGoals = [10,20,30];
+
+export default [10,20,30];
