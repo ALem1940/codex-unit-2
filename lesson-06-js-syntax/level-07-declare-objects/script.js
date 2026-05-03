056ad8642d73e9e12e04c3a6cd4bf44b1258b621
Ashanti <ashantilemonia@gmail.com>
2026-05-03 08:37:28 -0500

    finalized L6 L7

--- lesson-06-js-syntax/level-07-declare-objects/script.js
+++ lesson-06-js-syntax/level-07-declare-objects/script.js
@@ -4,6 +4,63 @@
 // Example:
 // let userProfile = { name: "Ava", age: 28, isSubscriber: false };
 
-let userProfile = {};
+let userProfile = {
+    name: "Ava", 
+    age: 28, 
+    isSubscriber: false
+};
+let userName = {
+    name:"Josh", 
+    age: 47, 
+    hasChosen: true};
+
+let studentEmail = {
+    name: "Amber", 
+    age: 10, 
+    isUsing: false};
+
+let carParts = {
+    engine: "transmission",
+    tires: 4,
+    lights: 2,
+    isReady: true
+};
+
+let libraryCard = {
+    genre: "inspirational",
+    zone: 2,
+    hasKidSection: false
+};
+
+let famUnion = {
+    shouldSchedule: "games",
+    adult: 100,
+    food: "BBQ"
+};
+
+let tutorAppointment = {
+    time: "afternoon",
+    tools: 3,
+    shouldBring: "computer"
+};
+
+let hobbiesPursued = {
+    sports: "baseball",
+    kidsInvolved: 5,
+    canParticipate: false
+};
+
+let orientationDay = {
+    cook: "dish",
+    amount: 4,
+    isWarm: true
+};
+
+let communityMeeting = {
+    attending: "community leaders",
+    month: "May",
+    isSchedule: true
+};
+
 
 export default userProfile;
