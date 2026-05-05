a589dd16881c8808bc0b5798c1cefe1f09c5660d
Ashanti <ashantilemonia@gmail.com>
2026-05-04 20:28:15 -0500

    finalized L6 L10

--- lesson-06-js-syntax/level-10-declare-functions/script.js
+++ lesson-06-js-syntax/level-10-declare-functions/script.js
@@ -4,3 +4,45 @@
 function getUser() {
   return "returns the current user";
 }
+
+function payBill () {
+  return "Bill is paid";
+}
+
+function callSteven () {
+  return "Steven already called";
+}
+
+function doChores () {
+  return "Kids haven't done chores";
+}
+
+function planParty () {
+  return "Party like a rockstar!";
+}
+
+function cookDinner () {
+  return "Everybody's welcome!";
+}
+
+function makeTime () {
+  return "Every mom needs a spa day!";
+}
+
+function readBible () {
+  return "Everything you need is in that book!";
+}
+
+function getUp () {
+  return "Everyone needs exercise!";
+}
+
+function washCar () {
+  return "A little sun never hurts anyone";
+}
+
+function setMeeting () {
+  return "Zoom set with teachers"
+}
+
+export default readBible;
