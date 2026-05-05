3701ae55be842868428dba9d697695291a4cf740
Ashanti <ashantilemonia@gmail.com>
2026-05-05 17:08:41 -0500

    changed L6 L13L14

--- lesson-06-js-syntax/level-13-for-loops/script.js
+++ lesson-06-js-syntax/level-13-for-loops/script.js
@@ -12,7 +12,7 @@ for (let bedTime=8; bedTime > 5; bedTime--) {
     console.log(bedTime);
 }
 
-for (let moveForward=100; moveForward >= 99; moveForward++) {
+for (let moveForward=100; moveForward >= 99; moveForward--) {
     console.log(moveForward);
     }
 
@@ -20,22 +20,22 @@ for (let gumbo=20; gumbo <=30; gumbo = gumbo + 4) {
     console.log(gumbo);    
 }
 
-for (point=5; point > 0; point++) {
+for (let point=5; point > 0; point--) {
     console.log(point);
 }
 
-for (apples=12; apples <=22; apples += 10) {
+for (let apples=12; apples <=22; apples += 10) {
     console.log(apples);
 }
 
-for (bags=42; bags < 50; bags++ ) {
+for (let bags=42; bags < 50; bags++ ) {
     console.log(bags);
 }
 
-for (candy=59; candy <= 60; candy-=5) {
+for (let candy=59; candy <= 60; candy+=5) {
     console.log(candy);
 }
 
-for (socks=28;socks >=28; socks = socks + 8) {
+for (let socks=0;socks <=20; socks = socks + 8) {
     console.log(socks);
 }
