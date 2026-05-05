f8815cf3f66427032d510f5a37113b0a71714dec
Ashanti <ashantilemonia@gmail.com>
2026-05-04 22:29:00 -0500

    finalized L6 L12

--- lesson-06-js-syntax/level-12-debug-functions/script.js
+++ lesson-06-js-syntax/level-12-debug-functions/script.js
@@ -1,33 +1,34 @@
 // Level 12 — Debug function declarations and calls
 // TODO: Fix five broken function declarations and their corresponding calls.
 
-function sayHi { 
+function sayHi () { 
   return "hi"; 
 }
 
-sayHi[];
+sayHi();
 
-function add(a b) { 
+function add(a,b) { 
   return a + b; 
 }
 
-add(5);
+add();
 
 function getName() {
-  "Name"
+  return "Name";
 };
 
 getNname();
 
-function multiply(x, y) 
+function multiply(x, y) {
   return x * y;
-
-multiply 2,3);
-
-function (name) { 
-  return `Hello ${name}`; 
 }
 
-greetPerson("Ava"));
+multiply();
 
-export default greeet;
+function greet(name) { 
+  return "Hello" + name; 
+}
+
+greet("Ava");
+
+export default greet;
