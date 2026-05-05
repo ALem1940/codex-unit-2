1b97e214025e47947901b3fd3166f0cff22b5402
Ashanti <ashantilemonia@gmail.com>
2026-05-04 21:29:24 -0500

    finalized L6 L11

--- lesson-06-js-syntax/level-11-function-calls/script.js
+++ lesson-06-js-syntax/level-11-function-calls/script.js
@@ -1,8 +1,20 @@
 // Level 11 — Call provided function with args (10 calls)
 // Helper provided for students; call it 10 times and save results to descriptive consts.
 
+
 export function reportScore(playerName, score) {
   return playerName + " scored " + score + " points";
 }
 
 const avaScoreReport = reportScore("Ava", 0);
+const stevenScoreReport = reportScore("Steven",100);
+const believeScoreReport = reportScore("Believe",120);
+const arsonScoreReport = reportScore("Arson",120);
+const marcelScoreReport = reportScore("Marcel",110);
+const ashantiScoreReport = reportScore("Ashanti", 100);
+const zazScoreReport = reportScore("Zaz", 105);
+const louScoreReport = reportScore("Lou",105);
+const totScoreReport = reportScore("Tot",105);
+const keyScoreReport = reportScore("Key",110);
+
+export default stevenScoreReport;
