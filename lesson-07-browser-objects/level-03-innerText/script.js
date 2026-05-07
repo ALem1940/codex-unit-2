c75c4acb4a9ab0639dce3b34641b6a8bca44f8fc
Ashanti <ashantilemonia@gmail.com>
2026-05-07 17:02:13 -0500

    completed L7 L3

--- lesson-07-browser-objects/level-03-innerText/script.js
+++ lesson-07-browser-objects/level-03-innerText/script.js
@@ -1,7 +1,10 @@
 // Level 03 — innerText starter
 // TODO: select an element (e.g. document.getElementById('desc'))
+const pTag = document.getElementById("desc");
 // TODO: console.log the previous innerText, set a new innerText, then log the new value
-
+console.log(pTag.innerText);
+pTag.innerText = "Hello world!";
+console.log(pTag.innerText);
 // Scaffold:
 // const el = document.getElementById('desc');
 // console.log('before:', el && el.innerText);
