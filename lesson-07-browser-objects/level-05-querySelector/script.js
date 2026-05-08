f4dff604e5d2f731d7ea8afc7fca7ba00a75244e
Ashanti <ashantilemonia@gmail.com>
2026-05-08 05:57:52 -0500

    Un-completed L7 L5

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,6 +1,10 @@
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
+const pTag = document.querySelector("p");
+
 // TODO: log previous content and update nodes (iterate NodeList with forEach)
+pTag.innerHTML = "<h2>Hello world!</h2>";
+
 
 // Scaffold:
 // const one = document.querySelector('#content');
