9048feed366f0ecf82e0b0b99cb6cc4c3bf7c76c
Ashanti <ashantilemonia@gmail.com>
2026-05-10 21:15:03 -0500

    finished L8 level 1

--- lesson-08-rest-api/level-01/script.js
+++ lesson-08-rest-api/level-01/script.js
@@ -1,4 +1,5 @@
 // Level 01 starter script
 // TODO: Select a target element and set its innerText to explain REST API.
 const result = document.getElementById("result");
+result.innerText = "A REST API is a way for my webpage to communicate with servers to get data like map directions weather forecast, and store inventory/product prices."
 // Example: result.innerText = '...'; // fill this in during the exercise
