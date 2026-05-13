485f1fd4b7495e0ca26d7aeda09f2b4b4fa57180
Ashanti <ashantilemonia@gmail.com>
2026-05-12 21:09:43 -0500

    finished L8 level 3

--- lesson-08-rest-api/level-03/script.js
+++ lesson-08-rest-api/level-03/script.js
@@ -6,6 +6,9 @@ const result = document.getElementById("result");
 async function handleSubmit(event) {
   event.preventDefault();
   // TODO: use async/await to call fetch('https://api.jsoning.com/mock/public/users') and then set result.innerText
+  const response = await fetch("https://postman-echo.com/get");
+  const pTag = document.getElementById("result");
+  pTag.innerText = "Async marks a function as asynchronous or takes time. Await is used to wait for fetch to finish. It pauses execution."
 }
 
 if (form) form.onsubmit = handleSubmit;
