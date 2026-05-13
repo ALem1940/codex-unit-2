c37256ed2bb5a6bdf675b175013fca8def4001f0
Ashanti <ashantilemonia@gmail.com>
2026-05-12 22:07:19 -0500

    finished L8 level 4

--- lesson-08-rest-api/level-04/script.js
+++ lesson-08-rest-api/level-04/script.js
@@ -1,11 +1,15 @@
 // Level 04 starter script
 // TODO: Await fetch and call response.json() to parse the body.
 const form = document.getElementById("sample-form");
-const result = document.getElementById("result");
+form.onsubmit = handleSubmit;
+
 
 async function handleSubmit(event) {
   event.preventDefault();
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = response.json();
+  const pTag = document.getElementById("result");
   // TODO: fetch and parse JSON, then set result.innerText with a short explanation
 }
 
-if (form) form.onsubmit = handleSubmit;
+
