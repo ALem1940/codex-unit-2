3421e02625069a8c6c374fe895e080fb6112f3d8
Ashanti <ashantilemonia@gmail.com>
2026-05-13 14:21:49 -0500

    updated errors

--- lesson-05-terminal-commands/level-10/answer.js
+++ lesson-05-terminal-commands/level-10/answer.js
@@ -1,3 +1,3 @@
 // Level 10 — Challenge: Multi-step script run
 // TODO: Export a string array with the VS Code steps and terminal commands you used.
-export default [];
+export default ["node ./folder/script.js","clear","cd ..", "added file", "created folder","moved file",];
