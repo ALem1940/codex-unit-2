31d58d96c1c2452b6cc090f071d55caf5dcc48ae
Ashanti <ashantilemonia@gmail.com>
2026-05-14 03:42:10 -0500

    worked L8 level 5

--- lesson-08-rest-api/level-05/script.js
+++ lesson-08-rest-api/level-05/script.js
@@ -1,20 +1,21 @@
 // Level 05 starter script
 // TODO: Fetch GET https://api.jsoning.com/mock/public/users, await response.json(), and read display the data.
 const form = document.getElementById("sample-form");
+
 form.onsubmit = handleSubmit;
 
 async function handleSubmit(e) {
   e.preventDefault();
-  response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const userdata = await response.json();
   const userId = userdata.userId;
   const title = userdata.title;
-  const isCompleted = userdata.Completed;
-  const pTag1 = document.getElementById("username");
-  const pTag2 = document.getElementById("email");
-  const pTag3 = document.getElementById("id");
+  const isCompleted = userdata.completed;
+  const pTag1 = document.getElementById("url");
+  const pTag2 = document.getElementById("host");
+  const pTag3 = document.getElementById("encoding");
   pTag1.innerText = "User ID: " + userId;
-  pTag2.innerText = "Title: " + title;
+  pTag2.innerText = "TITLE: " + title;
   pTag3.innerText = "Is Completed: " + isCompleted;
   // TODO: implement fetch and set the three elements' innerText
 }
