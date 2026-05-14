03150e16bd33df318d76be2b245e6eb703ab5710
Ashanti <ashantilemonia@gmail.com>
2026-05-13 19:04:46 -0500

    added updates-error

--- lesson-07-browser-objects/level-05-querySelector/script.js
+++ lesson-07-browser-objects/level-05-querySelector/script.js
@@ -1,16 +1,9 @@
 // Level 05 — querySelector starter
 // TODO: use document.querySelector('#content') or document.querySelectorAll('.item')
-const pTag = document.querySelector("#content");
+const div = document.querySelector("#content");
 
 // TODO: log previous content and update nodes (iterate NodeList with forEach)
-pTag.innerHTML = "<h2>the innerText/innerHTML is used to change text content on the page! one changes the text and the other makes changes to the html</h2>";
+div.innerHTML = `<h2>The innerText/innerHTML is used to change text content on the page! One changes the text and the other makes changes to the html.</h2>`;
 
 
-// Scaffold:
-// const one = document.querySelector('#content');
-// console.log('before:', one && one.innerHTML);
-// // TODO: one.innerHTML = '...';
-// const list = document.querySelectorAll('.item');
-// list.forEach((el, i) => {
-//   // TODO: update each el.innerText
-// });
+
