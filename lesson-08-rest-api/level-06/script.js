0291f74e4a5e026bb07dc00a242f7ce430590ac9
Ashanti <ashantilemonia@gmail.com>
2026-05-14 12:50:48 -0500

    finished L8 level 6

--- lesson-08-rest-api/level-06/script.js
+++ lesson-08-rest-api/level-06/script.js
@@ -1,12 +1,21 @@
 // Level 06 starter script
 // TODO: Use form.elements (by index) to collect input values into a plain object.
 const form = document.getElementById("sample-form");
-const out = document.getElementById("data");
 
-function handleSubmit(e) {
-  e.preventDefault();
+form.onsubmit = handleSubmit;
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = {
+    username: form.elements.username.value,
+    email: form.elements.email.value
+  };
+  
+  const out = document.getElementById("data");
   // TODO: build a `data` object from form.elements using indexes (e.g. form.elements[0].value)
   // Example display: out.innerText = JSON.stringify(data, null, 2);
 }
 
-if (form) form.onsubmit = handleSubmit;
+
