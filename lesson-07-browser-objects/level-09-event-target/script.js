2a69ef9d5f3a02b1159db3cd6d65981641d9ff63
Ashanti <ashantilemonia@gmail.com>
2026-05-13 19:06:24 -0500

    added updates-error

--- lesson-07-browser-objects/level-09-event-target/script.js
+++ lesson-07-browser-objects/level-09-event-target/script.js
@@ -1,14 +1,15 @@
 // Level 09 — event.target starter
 // TODO: inside submit handler inspect event.target and event.currentTarget
-const form = document.getElementById("sample-form");
-form.onsubmit = handleSubmit;
+//const form = document.getElementById("sample-form");
+//form.onsubmit = handleSubmit;
 
 function handleSubmit(event) {
   // TODO: call event.preventDefault() when experimenting
   event.preventDefault();
-  const formTag = event.target;
-  console.log(formTag.id);
+  const form = event.target;
+  console.log("event.target is the element that originally sent the event.",form);
   // TODO: const form = event.target; use form.elements to access inputs
+  debugger;
 }
 
 // const form = document.getElementById('sample-form');
